05b49f6edafd49dedb9a00f759be686ce48cc232
Ville Peurala <vpeurala@91-157-60-189.elisa-laajakaista.fi>
2016-08-31 12:09:54 +0300

    Almost migrated from Elm 0.16 to 0.17. WIP

--- src/Main.elm
+++ src/Main.elm
@@ -1,246 +1,223 @@
-module Main (..) where
+module Main exposing (..)
 
 import Char exposing (KeyCode, fromCode, isLower, isUpper)
-import Debug exposing (..)
-import Effects exposing (Effects, Never)
 import Html exposing (..)
-import Html.Attributes exposing (autofocus, class, tabindex, value)
+import Html.App as Html
+import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Random exposing (initialSeed, int, generate, list)
-import Signal exposing (Address)
 import String exposing (fromChar, toUpper)
 import Task exposing (Task)
 import Time exposing (Time)
-import TimeApp exposing (App, start)
 import Associations exposing (..)
 
 
 type Model
-  = Initial
-  | Running GameState
-  | Paused GameState
-  | Over GameState
+    = Initial
+    | Running GameState
+    | Paused GameState
+    | Over GameState
 
 
 type alias GameState =
-  { done : List Association
-  , left : List Association
-  , current : Association
-  , input : String
-  , timer : Float
-  }
+    { done : List Association
+    , left : List Association
+    , current : Association
+    , input : String
+    , timer : Float
+    }
 
 
-type Action
-  = NoOp
-  | StartOrPause
-  | Input Char
-  | Backspace
-  | Tick
+type Msg
+    = NoOp
+    | StartOrPause
+    | Input Char
+    | Backspace
+    | Tick
 
 
 shuffle : List a -> Int -> List a
 shuffle xs seed =
-  let
-    gen =
-      list (List.length xs) (int 0 1000)
+    let
+        gen =
+            Random.list (List.length xs) (int 0 1000)
 
-    seed' =
-      initialSeed seed
+        seed' =
+            initialSeed seed
 
-    ( rands, _ ) =
-      generate gen seed'
+        ( rands, _ ) =
+            generate gen seed'
 
-    zips =
-      List.map2 (,) xs rands
+        zips =
+            List.map2 (,) xs rands
 
-    sorted =
-      List.sortBy snd zips
-  in
-    List.map fst sorted
+        sorted =
+            List.sortBy snd zips
+    in
+        List.map fst sorted
 
 
-keyboard : Int -> Action
+keyboard : Int -> Msg
 keyboard x =
-  case x of
-    32 ->
-      StartOrPause
+    case x of
+        32 ->
+            StartOrPause
 
-    8 ->
-      Backspace
+        8 ->
+            Backspace
 
-    222 ->
-      Input 'Ä'
+        222 ->
+            Input 'Ä'
 
-    186 ->
-      Input 'Ö'
+        186 ->
+            Input 'Ö'
 
-    c ->
-      let
-        ch =
-          fromCode c
-      in
-        if isUpper ch then
-          Input ch
-        else
-          NoOp
+        c ->
+            let
+                ch =
+                    fromCode c
+            in
+                if isUpper ch then
+                    Input ch
+                else
+                    NoOp
 
 
 startGame : GameState
 startGame =
-  case (shuffle allAssociations 1178) of
-    [] ->
-      Debug.crash "No associations!"
+    case (shuffle allAssociations 1178) of
+        [] ->
+            Debug.crash "No associations!"
 
-    x :: xs ->
-      { done = []
-      , left = xs
-      , current = x
-      , input = ""
-      , timer = 0
-      }
+        x :: xs ->
+            { done = []
+            , left = xs
+            , current = x
+            , input = ""
+            , timer = 0
+            }
 
 
-init : ( Model, Effects Action )
+init : ( Model, Cmd Msg )
 init =
-  ( Initial
-  , Effects.none
-  )
-
-
-noFx : Model -> ( Model, Effects Action )
-noFx m =
-  ( m, Effects.none )
-
-
-update : Action -> Time -> Model -> ( Model, Effects Action )
-update action time model =
-  noFx
-    (case ( action, model ) of
-      ( NoOp, model ) ->
-        model
-
-      ( StartOrPause, Initial ) ->
-        Running startGame
-
-      ( StartOrPause, Running state ) ->
-        Paused state
-
-      ( StartOrPause, Paused state ) ->
-        Running state
-
-      ( StartOrPause, Over state ) ->
-        Running startGame
-
-      ( Input c, Running state ) ->
-        let
-          input' =
-            state.input ++ (String.fromChar c)
-        in
-          if toUpper input' == toUpper state.current.word then
-            case state.left of
-              [] ->
-                Over { state | input = input' }
-
-              x :: xs ->
-                Running { state | input = "", current = x, left = xs, done = state.current :: state.done }
-          else
-            Running { state | input = input' }
-
-      ( Input c, model ) ->
-        model
-
-      ( Backspace, Running state ) ->
-        let
-          input' =
-            String.slice 0 -1 state.input
-        in
-          Running { state | input = input' }
-
-      ( Backspace, model ) ->
-        model
-
-      ( Tick, Running state ) ->
-        Running { state | timer = state.timer + 1 }
-
-      ( Tick, model ) ->
-        model
+    ( Initial
+    , Cmd.none
     )
 
 
-viewRunning : Address Action -> GameState -> Html
-viewRunning address state =
-  div
-    []
-    [ div [ class ("info running") ] [ text state.current.number ]
-    , input
-        [ autofocus True
-        , onWithOptions
-            "keydown"
-            { preventDefault = True, stopPropagation = True }
-            keyCode
-            (\s -> Signal.message address (keyboard s))
-        , value state.input
-        ]
+update : Msg -> Model -> ( Model, Cmd Msg )
+update action model =
+    case ( action, model ) of
+        ( NoOp, model ) ->
+            model
+
+        ( StartOrPause, Initial ) ->
+            Running startGame
+
+        ( StartOrPause, Running state ) ->
+            Paused state
+
+        ( StartOrPause, Paused state ) ->
+            Running state
+
+        ( StartOrPause, Over state ) ->
+            Running startGame
+
+        ( Input c, Running state ) ->
+            let
+                input' =
+                    state.input ++ (String.fromChar c)
+            in
+                if toUpper input' == toUpper state.current.word then
+                    case state.left of
+                        [] ->
+                            Over { state | input = input' }
+
+                        x :: xs ->
+                            Running { state | input = "", current = x, left = xs, done = state.current :: state.done }
+                else
+                    Running { state | input = input' }
+
+        ( Input c, model ) ->
+            model
+
+        ( Backspace, Running state ) ->
+            let
+                input' =
+                    String.slice 0 -1 state.input
+            in
+                Running { state | input = input' }
+
+        ( Backspace, model ) ->
+            model
+
+        ( Tick, Running state ) ->
+            Running { state | timer = state.timer + 1 }
+
+        ( Tick, model ) ->
+            model
+
+
+viewRunning : GameState -> Html Msg
+viewRunning state =
+    div
         []
-    , div [] [ text (toString state.timer) ]
-    ]
-
-
-viewDiv : Address Action -> String -> String -> String -> Html
-viewDiv address statusText inputValue statusClass =
-  div
-    []
-    [ div [ class ("info " ++ statusClass) ] [ text statusText ]
-    , input
-        [ autofocus True
-        , onWithOptions
-            "keydown"
-            { preventDefault = True, stopPropagation = True }
-            keyCode
-            (\s -> Signal.message address (keyboard s))
-        , value inputValue
+        [ div [ class ("info running") ] [ text state.current.number ]
+        , input
+            [ autofocus True
+            , onWithOptions
+                "keydown"
+                { preventDefault = True, stopPropagation = True }
+                keyCode
+            , value state.input
+            ]
+            []
+        , div [] [ text (toString state.timer) ]
         ]
+
+
+viewDiv : String -> String -> String -> Html Msg
+viewDiv statusText inputValue statusClass =
+    div
         []
-    ]
+        [ div [ class ("info " ++ statusClass) ] [ text statusText ]
+        , input
+            [ autofocus True
+            , onWithOptions
+                "keydown"
+                { preventDefault = True, stopPropagation = True }
+                keyCode
+            , value inputValue
+            ]
+            []
+        ]
 
 
-view : Address Action -> Model -> Html
-view address model =
-  case model of
-    Initial ->
-      viewDiv address "Paina välilyöntiä aloittaaksesi" "" "initial"
+view : Model -> Html Msg
+view model =
+    case model of
+        Initial ->
+            viewDiv "Paina välilyöntiä aloittaaksesi" "" "initial"
 
-    Running state ->
-      viewRunning address state
+        Running state ->
+            viewRunning state
 
-    Paused state ->
-      viewDiv address "Pysäytetty, paina välilyöntiä jatkaaksesi" state.input "paused"
+        Paused state ->
+            viewDiv "Pysäytetty, paina välilyöntiä jatkaaksesi" state.input "paused"
 
-    Over state ->
-      viewDiv address "Peli on loppu, paina välilyöntiä aloittaaksesi uuden" state.input "over"
+        Over state ->
+            viewDiv "Peli on loppu, paina välilyöntiä aloittaaksesi uuden" state.input "over"
 
 
 const : b -> (a -> b)
 const x =
-  \_ -> x
+    \_ -> x
 
 
-app : App Model
-app =
-  start
-    { init = init
-    , inputs = [ Signal.map (const Tick) (Time.fps 24) ]
-    , update = update
-    , view = view
-    }
-
-
-main : Signal Html
 main =
-  app.html
-
-
-port tasks : Signal (Task Never ())
-port tasks =
-  app.tasks
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
