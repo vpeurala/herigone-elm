7316e3e99b3ea32a23d3dc33a57e313bf0d78ea5
Ville Peurala <ville.peurala@gmail.com>
2017-01-21 17:49:09 +0200

    Add index.html back

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-scp index.html root@95.85.25.96:/var/www/html/
-scp elm.js root@95.85.25.96:/var/www/html/
-scp herigone.css root@95.85.25.96:/var/www/html/
+./build.sh
+scp index.html elm.js herigone.css root@95.85.25.96:/var/www/html/
 
